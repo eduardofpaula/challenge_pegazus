3f6eb7a5cf498d4206863839aa3f4354f4306a01
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-11-06 02:13:27 -0300

    feat: assets and fonts

--- fitclub/tailwind.config.cjs
+++ fitclub/tailwind.config.cjs
@@ -1,18 +1,18 @@
 /* eslint-env node */
-// const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme')
 const { iconsPlugin, getIconCollections } = require('@egoist/tailwindcss-icons')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,ts}'],
-  // theme: {
-  //   extend: {
-  //     // here's how to extend fonts if needed
-  //     fontFamily: {
-  //       sans: [...defaultTheme.fontFamily.sans],
-  //     },
-  //   },
-  // },
+  theme: {
+    extend: {
+      // here's how to extend fonts if needed
+      fontFamily: {
+        sans: ["'Poppins'",...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
