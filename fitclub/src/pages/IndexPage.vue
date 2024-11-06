336af7722d2bf283ecdcaee884921c8ba91f5cb4
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-11-06 04:12:56 -0300

    feat: navbar

--- fitclub/src/pages/IndexPage.vue
+++ fitclub/src/pages/IndexPage.vue
@@ -2,7 +2,53 @@
 const store = useStore()
 </script>
 <template>
-  <header>
-    <div class="bg-gray-500 h-96"></div>
+  <header class="h-screen bg-gradient-to-tr from-cinza1 via-cinza2 to-laranja2">
+    <div class="flex items-center justify-between px-6 py-4 mx-60">
+      <div>
+        <img src="@/assets/logo.png" alt="logo" class="w-100" />
+      </div>
+
+      <nav class="flex flex-grow justify-center mt-8">
+        <ul class="flex space-x-6 text-branco">
+          <li>
+            <a href="#home" class="navButton">Home</a>
+          </li>
+          <li>
+            <a href="#about" class="navButton">Program</a>
+          </li>
+          <li>
+            <a href="#services" class="navButton">Service</a>
+          </li>
+          <li>
+            <a href="#contact" class="navButton">About</a>
+          </li>
+          <li>
+            <a href="#community" class="navButton">Community</a>
+          </li>
+        </ul>
+      </nav>
+
+      <a href="#dourado" class="goldButton">Join Now</a>
+    </div>
   </header>
 </template>
+
+<style scoped>
+.navButton {
+  @apply hover:text-laranja2 transition duration-300;
+}
+
+.goldButton {
+  background-color: #f9ac54;
+  color: white;
+  border: none;
+  padding: 12px 24px;
+  font-size: 16px;
+  font-weight: bold;
+  border-radius: 10px;
+  transition:
+    background-color 0.3s ease,
+    transform 0.3s ease;
+  cursor: pointer;
+}
+</style>
--- fitclub/tailwind.config.cjs
+++ fitclub/tailwind.config.cjs
@@ -9,7 +9,16 @@ module.exports = {
     extend: {
       // here's how to extend fonts if needed
       fontFamily: {
-        sans: ["'Poppins'",...defaultTheme.fontFamily.sans],
+        sans: ["'Poppins'", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        cinza1: '#111317',
+        cinza2: '#1f2125',
+        cinza3: '#35373b',
+        laranja1: '#f9ac54',
+        laranja2: '#d79447',
+        cinza4: '#d1d5db',
+        branco: '#ffffff',
       },
     },
   },
