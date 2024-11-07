1a86829a85d5b37b17cb6a183c480145b0832500
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2024-11-06 21:42:32 -0300

    feat: cards to differents programs

--- fitclub/src/pages/IndexPage.vue
+++ fitclub/src/pages/IndexPage.vue
@@ -1,4 +1,71 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const posts = ref([
+  {
+    id: 1,
+    title: 'Strength Training',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. UllamLorem ipsum dolor sit amet consectetur adipisicing elit. Ullam',
+    href: '#',
+  },
+  {
+    id: 2,
+    title: 'Physical Fitness',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. UllamLorem ipsum dolor sit amet consectetur adipisicing elit. Ullam',
+    href: '#',
+  },
+  {
+    id: 3,
+    title: 'Fat Lose',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. UllamLorem ipsum dolor sit amet consectetur adipisicing elit. Ullam',
+    href: '#',
+  },
+  {
+    id: 4,
+    title: 'Weight Gain',
+    description:
+      'Dolor sit amet, consectetur adipiscing elit. Quisque euismod, lorem ac vehicula accumsan, nunc nunc vehicula ipsum.',
+    href: '#',
+  },
+  {
+    id: 5,
+    title: 'Cardio',
+    description:
+      'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid.',
+    href: '#',
+  },
+  {
+    id: 6,
+    title: 'Yoga',
+    description:
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    href: '#',
+  },
+])
+
+const icons = ref([
+  'src/assets/peso.png',
+  'src/assets/forca.png',
+  'src/assets/fat.png',
+  'src/assets/gain.png',
+  'src/assets/cardio.png',
+  'src/assets/yoga.png',
+])
+
+const visibleIndex = ref(0)
+
+const nextSlide = () => {
+  visibleIndex.value = (visibleIndex.value + 3) % posts.value.length
+}
+
+const prevSlide = () => {
+  visibleIndex.value = (visibleIndex.value - 3 + posts.value.length) % posts.value.length
+}
+</script>
+
 <template>
   <div class="bg-gradient">
     <header class="h-full">
@@ -49,8 +116,53 @@
     <div class="flex flex-row justify-between mx-80 mt-40">
       <div class="explore">EXPLORE YOUR PROGRAM</div>
       <div class="flex flex-row">
-        <img width="50" height="50" src="@/assets/arrow.svg" alt="circled-left-2" />
-        <img width="50" height="50" src="@/assets/arrow2.svg" alt="circled-right-2" />
+        <img
+          width="50"
+          height="50"
+          src="@/assets/arrow.svg"
+          alt="circled-left-2"
+          class="cursor-pointer"
+          @click="prevSlide"
+        />
+        <img
+          width="50"
+          height="50"
+          src="@/assets/arrow2.svg"
+          alt="circled-right-2"
+          class="cursor-pointer"
+          @click="nextSlide"
+        />
+      </div>
+    </div>
+    <div class="flex flex-row justify-between mx-80 mt-10">
+      <div class="pb-24 w-full">
+        <div class="mx-auto max-w-7xl">
+          <div
+            class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-gray-200 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+          >
+            <article
+              v-for="(post, index) in posts.slice(visibleIndex, visibleIndex + 3)"
+              :key="post.id"
+              class="flex max-w-xl flex-col items-start justify-between border-2 border-laranja2 rounded-lg"
+            >
+              <div class="flex items-center gap-x-4 text-xs">
+                <img :src="icons[(visibleIndex + index) % icons.length]" alt="icon" />
+              </div>
+              <div class="group relative">
+                <h3 class="mt-3 text-lg/6 font-bold text-branco">
+                  <a :href="post.href">
+                    <span class="absolute inset-0" />
+                    {{ post.title }}
+                  </a>
+                </h3>
+                <p class="mt-5 line-clamp-3 text-sm/6 text-branco">{{ post.description }}</p>
+              </div>
+              <div class="relative mt-8 flex items-center gap-x-4">
+                <button class="goldButton">Join Now</button>
+              </div>
+            </article>
+          </div>
+        </div>
       </div>
     </div>
   </div>
